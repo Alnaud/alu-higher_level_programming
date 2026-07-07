8d44354ca33057130864309084403adfc2e88a1c
Alnaud <a.mugisha5@alustudent.com>
2026-07-07 03:06:46 -0700

    0

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for i in my_list:
-        print("{:d}".format(i))
+    """Print all integers of a list."""
+    for number in my_list:
+        print("{:d}".format(number))
